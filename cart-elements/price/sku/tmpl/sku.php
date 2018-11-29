feb073b2d9ada9e86a0c2c4968d34df2d9ba20eb
olivergreen <inte@yandex.ru>
2018-11-29 18:03:44 +0200

    Add Cart elements SKU template

---
+++ cart-elements/price/sku/tmpl/sku.php
@@ -0,0 +1,21 @@
+<?php
+/**
+* JBZoo App is universal Joomla CCK, application for YooTheme Zoo component
+*
+* @package jbzoo
+* @version 2.x Pro
+* @author JBZoo App http://jbzoo.com 
+* @copyright Copyright (C) JBZoo.com, All rights reserved.
+* @license http://jbzoo.com/license-pro.phpJBZoo Licence
+* @coder Alexander Oganov <t_tapak@yahoo.com>;
+* @coder Sergey Kalistratov <kalistratov.s.m@gmail.com>;
+*/
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+$sku = $this->getValue(true);
+
+if ($sku) : ?>
+    <span class="sku uk-badge"><?php echo $sku; ?></span>
+<?php endif;
