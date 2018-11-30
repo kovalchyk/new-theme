e60da246e5dd885ef9969e77fdc541e971010a40
olivergreen <inte@yandex.ru>
2018-12-01 00:29:11 +0200

    Fix Related template Slider's layout

--- renderer/item/bouquets/related.php
+++ renderer/item/bouquets/related.php
@@ -23,14 +23,8 @@ $align = $this->app->jbitem->getMediaAlign($item, $layout);
     <div class="uk-cover-container uk-transition-toggle" tabindex="0">
         <!-- Элемент картинки -->
         <?php if ($this->checkPosition('image')) : ?>
-            <!-- <div class="item-image uk-align-<?php //echo $align; ?>"> -->
                 <?php echo $this->renderPosition('image'); ?>
-            <!-- </div> -->
         <?php endif; ?>
-        <!-- <img src="images/products/demo/demo-buket-4.jpg" srcset="images/products/demo/demo-buket-4.jpg 450w, images/products/demo/demo-buket-4.jpg 768w, images/products/demo/demo-buket-4.jpg 900w"
-            sizes="(min-width: 450px) 450px" data-width="1200" data-height="1440" class="el-image uk-transition-scale-up uk-transition-opaque"
-            alt> -->
-
         <div class="uk-overlay uk-position-bottom uk-overlay-default">
             <div class="el-content uk-margin">
                 <!-- Название итема -->
@@ -39,42 +33,39 @@ $align = $this->app->jbitem->getMediaAlign($item, $layout);
                         <h3 class="uk-h5 uk-heading-line uk-text-truncate uk-margin-remove"><?php echo $this->renderPosition('title'); ?></h5>
                     </div>
                 <?php endif; ?>
-
                 <div class="teaser-price-rating uk-child-width-1-2 uk-grid-small" uk-grid>
                     <!-- Цена итема -->
-                    <div class="price">
-                        <h3 class="uk-h4">17 000 <span class="">₽</span></h3>
-                    </div>
+                    <?php if ($this->checkPosition('price')) : ?>
+                        <div class="price">
+                            <h3 class="uk-h4"><?php echo $this->renderPosition('price', array('style' => 'default')); ?></h3>
+                        </div>
+                    <?php endif; ?>
                     <!-- Элемент рейтинга -->
                     <div class="teaser-rating">
                         <div class="uk-text-right">
                             <?php if ($this->checkPosition('rating')) : ?>
                                 <?php echo $this->renderPosition('rating', array('style' => 'block')); ?>
-                            <?php endif; ?>                        
+                            <?php endif; ?>
                         </div>
                     </div>
                 </div>
                 <div class="uk-margin-remove-bottom uk-grid-small uk-grid-margin-small" uk-grid>
                     <div class="uk-width-2-3 uk-first-column">
                         <div class="cart-button">
-
                             <?php if ($this->checkPosition('buttonbuy')) : ?>
                                 <div class="item-buttonbuy">
                                     <?php echo $this->renderPosition('buttonbuy', array('style' => 'block')); ?>
                                 </div>
                             <?php endif; ?>
-
                         </div>
                     </div>
                     <div class="uk-width-expand">
                         <div class="favorite-button uk-text-right">
-
                             <?php if ($this->checkPosition('favourite')) : ?>
                                 <div class="item-favourite">
                                     <?php echo $this->renderPosition('favourite', array('style' => 'block')); ?>
                                 </div>
                             <?php endif; ?>
-
                         </div>
                     </div>
                 </div>
