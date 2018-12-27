fc042d24357318f8a8be59242f1016a887dc0421
olivergreen <inte@yandex.ru>
2018-12-27 21:34:03 +0200

    Update rating.php
    fix conflict

--- elements/rating/tmpl/rating.php
+++ elements/rating/tmpl/rating.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 /**
  * @package   com_zoo
@@ -57,64 +56,4 @@ $id = $this->identifier.'-'.uniqid();
 			$('#<?php echo $id; ?>').ElementRating({ url: '<?php echo $link; ?>' });
 		});
 	</script>
-=======
-<?php
-/**
- * @package   com_zoo
- * @author    YOOtheme http://www.yootheme.com
- * @copyright Copyright (C) YOOtheme GmbH
- * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
- */
-
-// no direct access
-defined('_JEXEC') or die('Restricted access');
-
-// include assets js/css
-$this->app->document->addScript('elements:rating/assets/js/rating.js');
-$this->app->document->addStylesheet('elements:rating/assets/css/rating.css');
-
-$id = $this->identifier.'-'.uniqid();
-
-?>
-<div id="<?php echo $id; ?>" class="yoo-zoo rating">
-
-	<?php if ($show_message) : ?>
-	<div class="vote-message uk-margin-small">
-		<?php echo $rating.'/<strong>'.$stars.'</strong> '.JText::sprintf($votes == 1 ? 'rating %s vote' : 'rating %s votes', $votes); ?>
-	</div>
-	<?php endif; ?>
-
-	<div class="rating-container star<?php echo $stars; ?>">
-		<div class="previous-rating" style="width: <?php echo intval($rating / $stars * 100); ?>%;"></div>
-
-		<?php if (!$disabled) : ?>
-		<div class="current-rating">
-
-			<?php for($i = $stars; $i > 0; $i--) : ?>
-			<div class="stars star<?php echo $i; ?>" uk-tooltip="<?php echo $i.' '.JText::_('out of').' '.$stars; ?>"></div>
-			<?php endfor ?>
-
-		</div>
-		<?php endif; ?>
-	</div>
-
-	<?php if ($show_microdata) : ?>
-	<div itemscope itemtype="http://data-vocabulary.org/Review-aggregate">
-		<meta itemprop="itemreviewed" content="<?php echo $this->getItem()->name; ?>" />
-		<div itemprop="rating" itemscope itemtype="http://data-vocabulary.org/Rating">
-			<meta itemprop="average" content="<?php echo number_format($rating, 1); ?>" />
-			<meta itemprop="best" content="<?php echo $stars; ?>" />
-		</div>
-		<meta itemprop="votes" content="<?php echo $votes; ?>"/>
-	</div>
-	<?php endif; ?>
-
-</div>
-<?php if (!$disabled) : ?>
-	<script type="text/javascript">
-		jQuery(function($) {
-			$('#<?php echo $id; ?>').ElementRating({ url: '<?php echo $link; ?>' });
-		});
-	</script>
->>>>>>> a0252d963538a4f37289ad43a3381715a140f236
 <?php endif;
